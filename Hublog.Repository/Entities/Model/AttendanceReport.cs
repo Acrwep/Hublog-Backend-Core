13a25ec41a31151d8ded47cc9a3c5f04c420ff0c
sethu_hublog <actewpdeveloper@gmail.com>
2024-08-30 10:05:22 +0530

    model altered

--- Hublog.Repository/Entities/Model/AttendanceReport.cs
+++ Hublog.Repository/Entities/Model/AttendanceReport.cs
@@ -2,7 +2,8 @@
 {
     public class AttendanceReport
     {
-        public string Employee { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }   
         public DateTime InTime { get; set; }    
         public DateTime Out { get; set; }
         public DateTime TotalTime { get; set; }
