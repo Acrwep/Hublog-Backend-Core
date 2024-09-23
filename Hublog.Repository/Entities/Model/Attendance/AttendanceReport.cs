73241bea65b465a3faaae0d91b18151a224248a9
sethu_hublog <actewpdeveloper@gmail.com>
2024-09-23 17:20:36 +0530

    fix

--- Hublog.Repository/Entities/Model/Attendance/AttendanceReport.cs
+++ Hublog.Repository/Entities/Model/Attendance/AttendanceReport.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; } 
         public string First_Name { get; set; }
         public string Last_Name { get; set; }
-        public string FullName { get; set; }    
+        public string Full_Name { get; set; }       
         public DateTime InTime { get; set; }
         public DateTime Out { get; set; }
         public DateTime TotalTime { get; set; }
