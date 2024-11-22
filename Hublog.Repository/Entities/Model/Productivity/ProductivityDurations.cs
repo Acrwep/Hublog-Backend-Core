1a7e2cb29ba7ee671cf6564bd4d4f82a941f14c8
sethu_hublog <hublogbackenddev@gmail.com>
2024-11-22 16:27:53 +0530

    productivity parameter order changed

--- Hublog.Repository/Entities/Model/Productivity/ProductivityDurations.cs
+++ Hublog.Repository/Entities/Model/Productivity/ProductivityDurations.cs
@@ -9,8 +9,8 @@ namespace Hublog.Repository.Entities.Model.Productivity
     public class ProductivityDurations
     {
         public string TotalProductiveDuration { get; set; }
-        public string TotalUnproductiveDuration { get; set; }
         public string TotalNeutralDuration { get; set; }
+        public string TotalUnproductiveDuration { get; set; }
         public string TotalDuration { get; set; }
         public string AverageDuratiopn {  get; set; }
     }
