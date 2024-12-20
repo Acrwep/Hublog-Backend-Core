f9fc2afca4cad0488a2dbf467847569a83b5b289
hublog-backenddev <hublogbackenddev@gmail.com>
2024-12-20 17:42:02 +0530

    update attendence report

--- Hublog.Repository/Entities/DTO/CombinedUsageDto.cs
+++ Hublog.Repository/Entities/DTO/CombinedUsageDto.cs
@@ -6,6 +6,7 @@
         public string Details { get; set; }
         public int UserId { get; set; }
         public string FullName { get; set; }
+        public string TeamName { get; set; }
         public string TotalUsage { get; set; }
         public decimal UsagePercentage { get; set; }
     }
--- Hublog.Repository/Entities/Model/Attendance/AttendanceReport.cs
+++ Hublog.Repository/Entities/Model/Attendance/AttendanceReport.cs
@@ -3,13 +3,15 @@
     public class AttendanceReport
     {
         public int Id { get; set; } 
-        public string First_Name { get; set; }
-        public string Last_Name { get; set; }
         public string Full_Name { get; set; }   
         public string Team_Name { get; set; }
         public DateTime InTime { get; set; }
         public DateTime Out { get; set; }
         public DateTime TotalTime { get; set; }
+        public DateTime BreakDuration { get; set; }
+        public DateTime IdleDuration { get; set; }
+        public DateTime ActiveTime { get; set; }
+        public DateTime OnlineTime { get; set; }
         public DateTime AttendanceDate { get; set; }
         public string Remark { get; set; }
     }
