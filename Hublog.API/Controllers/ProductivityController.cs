c22a4b72396a236987c2c68cb51d33ee657d87b1
hublogbackenddeveloper <hublogbackenddeveloper@gmail.com>
2025-04-12 09:54:52 +0530

    Uncommand authorize in productivity trend

--- Hublog.API/Controllers/ProductivityController.cs
+++ Hublog.API/Controllers/ProductivityController.cs
@@ -233,7 +233,7 @@ namespace Hublog.API.Controllers
                 return BadRequest(ex.Message);
             }
         }
-        //[Authorize(Policy = CommonConstant.Policies.AdminPolicy)]
+        [Authorize(Policy = CommonConstant.Policies.AdminPolicy)]
         [HttpGet("GetProductivity_Trend")]
         public async Task<IActionResult> GetProductivity_Trend(int organizationId, int? teamId, [FromQuery] int? userId, [FromQuery] DateTime fromDate, [FromQuery] DateTime toDate)
         {
