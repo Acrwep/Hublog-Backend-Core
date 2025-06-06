98b686855ebe59467b34f77769a3b250c8ded372
hublogbackenddeveloper <hublogbackenddeveloper@gmail.com>
2025-06-06 15:21:52 +0530

    Minor change

--- Hublog.Repository/Entities/Model/Team.cs
+++ Hublog.Repository/Entities/Model/Team.cs
@@ -7,7 +7,7 @@
         public bool Active { get; set; } //(bit, not null)
         public string Description { get; set; } //(varchar(200), null)
         public int OrganizationId { get; set; } //(int, not null)
-        public int ShiftId { get; set; } //(int, not null)
+        public int? ShiftId { get; set; } //(int, not null)
         public Nullable<int> Parentid { get; set; } //(int, null)
     }
 }
